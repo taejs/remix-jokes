364164c6dbd8d17990b241e6d3a12181ea8cba65
taerim.shin <taerim.shin@navercorp.com>
2022-01-20 09:22:45 +0900

    add styles to /

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,25 @@
 import {LinksFunction} from '@remix-run/react/routeModules';
 import stylesUrl from '../styles/index.css';
+import {Link} from 'remix';
 
 export const links: LinksFunction = () => {
   return [{rel: 'stylesheet', href: stylesUrl}];
 };
 export default function IndexRoute() {
-  return <div>Hello Index Route</div>;
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
